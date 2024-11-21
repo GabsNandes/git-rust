c165faccd0dd6c383e400cdd5e21c8416643dbc6
GasbNandes <136932490+GabsNandes@users.noreply.github.com>
2024-11-21 18:24:32 -0300

    crud final

--- realcrud/src/main.rs
+++ realcrud/src/main.rs
@@ -28,38 +28,155 @@ const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
 fn create_user_menu(db_url: &str){
 
     let mut name = String::new();
-    let mut email = String::new();
 
 
     print!("name: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut name).expect("Failed to read line");
-
+    name = name.trim().to_string();
     
-    print!("Email: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut email).expect("Failed to read line");
+    let email = check_email();
 
 
     
-    post_user(&db_url, &name, &email);
+    match post_user(&db_url, &name, &email){
+
+        Ok(result)=>{
+
+            println!("Response: {}", result);
+        }
+        Err(e) => {
+            println!("Error creating user: {}", e);
+        }
+    }
 }
 
 fn read_user_menu(db_url: &str){
 
     let mut num = String::new();
+    let mut choice = String::new();
+
+    print!("1- By id, else all users: ");
+    
+    io::stdout().flush().unwrap();
+    
+    io::stdin().read_line(&mut num).expect("Failed to read line");
+
     
 
-    print!("ID: ");
-    io::stdout().flush().unwrap();
+    if choice == "1"{
 
+        
+
+        print!("ID: ");
+        io::stdout().flush().unwrap();
+    
+        io::stdin().read_line(&mut num).expect("Failed to read line");
+    
+        let num: i32 = num.trim().parse().expect("REASON");
+    
+        match get_user_by_id(&db_url, num) {
+            Ok(user_json) => {
+                println!("User found: {}", user_json);
+            }
+            Err(e) => {
+                println!("Error fetching user: {}", e);
+            }
+        } 
+
+    }else{
+
+        let (status_line, user_json) = get_all_users(&db_url);
+
+        println!("Status: {}", status_line);
+        println!("Response: {}", user_json);
+        
+
+    }
+    
+
+    
+}
+
+fn edit_user_menu(db_url: &str){
+
+    let mut num = String::new();
+    let mut choice = String::new();
+    let mut name = String::new();
+
+    let (status_line, user_json) = get_all_users(&db_url);
+
+    println!("Status: {}", status_line);
+    println!("Response: {}", user_json);
+
+    print!("Id: ");
+    
+    io::stdout().flush().unwrap();
+    
     io::stdin().read_line(&mut num).expect("Failed to read line");
 
     let num: i32 = num.trim().parse().expect("REASON");
 
-    get_user_by_id(&db_url, num);
+    print!("name: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+    name.trim();
+
+    
+    let email = check_email();
+
+    match edit_user_by_id(&db_url, num, &name, &email){
+
+        Ok(result)=>{
+
+            println!("Response: {}", result);
+        }
+        Err(e) => {
+            println!("Error creating user: {}", e);
+        }
+    }
+    
+
     
 }
+
+fn delete_user_menu(db_url: &str){
+
+    let mut num = String::new();
+
+    let (status_line, user_json) = get_all_users(&db_url);
+
+    println!("Status: {}", status_line);
+    println!("Response: {}", user_json);
+
+    print!("Id: ");
+    
+    io::stdout().flush().unwrap();
+    
+    io::stdin().read_line(&mut num).expect("Failed to read line");
+
+    
+    let num: i32 = num.trim().parse().expect("REASON");
+
+  
+    match delete_user_by_id(&db_url, num) {
+
+        Ok(result)=>{
+
+            println!("Response: {}", result);
+        }
+        Err(e) => {
+            println!("Error creating user: {}", e);
+        }
+    } 
+
+   
+
+    
+}
+
+
+//CLI functions
 
 fn post_user(db_url: &str, name: &str, email:&str) -> Result<String, Box<dyn Error>> {
     // Connect to the database
@@ -101,7 +218,75 @@ fn get_user_by_id(db_url: &str, user_id: i32) -> Result<String, Box<dyn Error>> 
         Ok(None) => Ok("User not found".to_string()), // Return a message if user doesn't exist
         Err(e) => Err(Box::new(e)), // Return error if query fails
     }
+
+
 }
+
+fn get_all_users(db_url: &str) -> (String, String) {
+
+    let mut client = match Client::connect(db_url, NoTls) {
+        Ok(client) => client,
+        Err(e) => return (INTERNAL_ERROR.to_string(), format!("Error connecting to the database: {}", e)),
+    };
+
+    let mut users = Vec::new();
+
+    match client.query("SELECT id, name, email FROM users", &[]) {
+        Ok(rows) => {
+            for row in rows {
+                users.push(User {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                });
+            }
+            (OK_RESPONSE.to_string(), serde_json::to_string(&users).unwrap())
+        }
+        Err(e) => {
+            (INTERNAL_ERROR.to_string(), format!("Error querying the database: {}", e))
+        }
+    }
+}
+
+fn edit_user_by_id(db_url: &str, user_id: i32, name: &str, email:&str) -> Result<String, Box<dyn Error>> {
+
+
+    let mut client = Client::connect(db_url, NoTls)?;
+    
+    
+    let result = client.execute(
+        "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+                    &[&name, &email, &user_id],
+    );
+
+    
+    match result {
+        Ok(_) => Ok("User edited successfully".to_string()),
+        Err(e) => Err(Box::new(e)), 
+    }
+    }
+
+fn delete_user_by_id(db_url: &str, user_id: i32) -> Result<String, Box<dyn Error>> {
+
+    let mut client = Client::connect(db_url, NoTls)?;
+
+
+    let result = client.execute(
+        "DELETE FROM users WHERE id = $1", &[&user_id],
+    );
+
+
+    match result {
+        Ok(_) => Ok("User deleted successfully".to_string()),
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+
+
+
+
+//Main:
 
 fn main() {
     // Retrieve DATABASE_URL at runtime
@@ -139,144 +324,15 @@ fn main() {
         match num {
             "1" => create_user_menu(&db_url),
             "2" => read_user_menu(&db_url),
+            "3" => edit_user_menu(&db_url),
+            "4" => delete_user_menu(&db_url),
             _ => {
                 println!("Saindo...");
                 break;
             },
         }
     }
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_client(stream, &db_url);
-            }
-            Err(e) => {
-                println!("Unable to connect: {}", e);
-            }
-        }
-    }
     
-}
-
-// Handle client requests
-fn handle_client(mut stream: TcpStream, db_url: &str) {
-    let mut buffer = [0; 1024];
-    let mut request = String::new();
-
-    match stream.read(&mut buffer) {
-        Ok(size) => {
-            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
-            println!("{}", request);
-            let (status_line, content) = match &*request {
-
-                
-                r if r.starts_with("POST /users") => handle_post_request(r, db_url),
-                r if r.starts_with("GET /users/") => handle_get_request(r, db_url),
-                r if r.starts_with("GET /users") => handle_get_all_request(r, db_url),
-                r if r.starts_with("PUT /users/") => handle_put_request(r, db_url),
-                r if r.starts_with("DELETE /users/") => handle_delete_request(r, db_url),
-                _ => (NOT_FOUND.to_string(), "404 not found".to_string()),
-            };
-
-            stream.write_all(format!("{}{}", status_line, content).as_bytes()).unwrap();
-        }
-        Err(e) => eprintln!("Unable to read stream: {}", e),
-    }
-}
-
-// Handle POST request
-fn handle_post_request(request: &str, db_url: &str) -> (String, String) {
-    match (get_user_request_body(&request), Client::connect(db_url, NoTls)) {
-        (Ok(user), Ok(mut client)) => {
-            client
-                .execute(
-                    "INSERT INTO users (name, email) VALUES ($1, $2)",
-                    &[&user.name, &user.email],
-                )
-                .unwrap();
-
-            (OK_RESPONSE.to_string(), "User created".to_string())
-        }
-        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
-    }
-}
-
-// Handle GET request
-fn handle_get_request(request: &str, db_url: &str) -> (String, String) {
-    match (get_id(&request).parse::<i32>(), Client::connect(db_url, NoTls)) {
-        (Ok(id), Ok(mut client)) => match client.query_one("SELECT * FROM users WHERE id = $1", &[&id]) {
-            Ok(row) => {
-                let user = User {
-                    id: row.get(0),
-                    name: row.get(1),
-                    email: row.get(2),
-                };
-
-                (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap())
-            }
-            _ => (NOT_FOUND.to_string(), "User not found".to_string()),
-        },
-        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
-    }
-}
-
-// Handle GET all request
-fn handle_get_all_request(_request: &str, db_url: &str) -> (String, String) {
-    match Client::connect(db_url, NoTls) {
-        Ok(mut client) => {
-            let mut users = Vec::new();
-
-            for row in client.query("SELECT id, name, email FROM users", &[]).unwrap() {
-                users.push(User {
-                    id: row.get(0),
-                    name: row.get(1),
-                    email: row.get(2),
-                });
-            }
-
-            (OK_RESPONSE.to_string(), serde_json::to_string(&users).unwrap())
-        }
-        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
-    }
-}
-
-// Handle PUT request
-fn handle_put_request(request: &str, db_url: &str) -> (String, String) {
-    match (
-        get_id(&request).parse::<i32>(),
-        get_user_request_body(&request),
-        Client::connect(db_url, NoTls),
-    ) {
-        (Ok(id), Ok(user), Ok(mut client)) => {
-            client
-                .execute(
-                    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
-                    &[&user.name, &user.email, &id],
-                )
-                .unwrap();
-
-            (OK_RESPONSE.to_string(), "User updated".to_string())
-        }
-        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
-    }
-}
-
-// Handle DELETE request
-fn handle_delete_request(request: &str, db_url: &str) -> (String, String) {
-    match (get_id(&request).parse::<i32>(), Client::connect(db_url, NoTls)) {
-        (Ok(id), Ok(mut client)) => {
-            let rows_affected = client.execute("DELETE FROM users WHERE id = $1", &[&id]).unwrap();
-
-            // If rows affected is 0, user not found
-            if rows_affected == 0 {
-                return (NOT_FOUND.to_string(), "User not found".to_string());
-            }
-
-            (OK_RESPONSE.to_string(), "User deleted".to_string())
-        }
-        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
-    }
 }
 
 // Database setup
@@ -303,3 +359,45 @@ fn get_id(request: &str) -> &str {
 fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
     serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
 }
+
+//utils
+
+#[allow(unused_assignments)]
+fn check_email() -> String{
+
+
+    let mut input = String::new(); 
+    let mut trimmed_input = String::new(); 
+
+    loop{
+        print!("email: ");
+        
+        io::stdout().flush().unwrap();
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        trimmed_input = input.trim().to_string();
+
+        if trimmed_input.ends_with(".com") {
+            trimmed_input = trimmed_input.strip_suffix(".com").unwrap().to_string();
+        } 
+
+    
+        if &input.trim() == &trimmed_input {
+            
+            println!("invalid email, try again");
+
+        }else{
+            
+            if input.contains("@") {
+                break
+            }else{
+                println!("Invalid Email");
+                continue
+            }
+        }
+    }
+
+    input.trim().to_string()
+    }
+
+    
